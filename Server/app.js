934236157181d23e08713c419e1e69508432fdf0
KwonSunJae <32411719+KwonSunJae@users.noreply.github.com>
2022-05-31 06:31:38 +0900

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -63,7 +63,7 @@ io.on('connection', (socket) => {
   });
 //기본 속성 설정
 app.set("port",process.env.PORT||3000);
-app.set("host","133.186.209.203"); //루프백
+ //루프백
 
 //웹 폴더에 저장된 모든 웹 페이지에 대한 정적 참조 허용
 app.use(static(__dirname)); //현재 폴더에 대한 정적 접근
